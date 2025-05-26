af7246edba98d4b73748db8a3b10551955083f34
User3426 <tristanrage.adresse@gmail.com>
2025-05-26 21:34:59 +0200

    Phase1 : Création des paquetages et classes, début de code de bddmanager

---
+++ habilitations2024/bddmanager/BddManager.cs
@@ -0,0 +1,56 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace habilitations2024
+{
+    public class BddManager
+    {
+        /// <summary>
+        /// instance unique de la classe
+        /// </summary>
+        private readonly MySqlConnection connection;
+        /// <summary>
+        /// Objet de connexion à la BDD à partir d'une chaîne de connexion
+        /// </summary>
+        private static BddManager instance = null;
+
+        private BddManager(string stringConnect)
+        {
+            connection = new MySqlConnection(stringConnect);
+            connection.Open();
+        }
+
+        /// <summary>
+        /// creation d'une instance unique de la classe
+        /// </summary>
+        /// <param name="stringConnect"></param>
+        /// <returns></returns>
+        public static BddManager getInstance(string stringConnect)
+        {
+            if (BddManager.instance == null)
+            {
+                BddManager.instance = new BddManager(stringConnect);
+            }
+            return BddManager.instance;
+        }
+
+        public void reqUpdate(string stringQuery, Dictionary<string, object> parametres)
+        {
+            MySqlCommand command = new MySqlCommand(stringQuery, connection);
+            if (!(parametres is null))
+            {
+                foreach (KeyValuePair<string, object> parametre in parametres)
+                {
+                    command.Parameters.Add(new MySqlParameter(parametre.Key, parametre.Value));
+                }
+            }
+            command.Prepare();
+            command.ExecuteNonQuery();
+
+        }
+    }
+}
